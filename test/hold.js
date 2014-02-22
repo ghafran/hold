e534cbc7f45f997286a3dd61239b202293956a16
Ghafran Abbas <ghafranabbas@gmail.com>
2014-02-22 17:37:48 +0000

    on error move next round 1

--- test/hold.js
+++ test/hold.js
@@ -17,12 +17,13 @@ describe('hold cases', function() {
     it('basic', function(done) {
 
         var hold = new Hold();
+        var data = new Date().getTime();
         hold(function(until) {
 
-            until(null, 'done');
+            until(null, data);
         }, function(err, result) {
 
-            expect(result).to.equal('done');
+            expect(result).to.equal(data);
             done();
         });
     });
@@ -30,12 +31,13 @@ describe('hold cases', function() {
     it('basic with expire option', function(done) {
 
         var hold = new Hold(1000);
+        var data = new Date().getTime();
         hold(function(until) {
 
-            until(null, 'done');
+            until(null, data);
         }, function(err, result) {
 
-            expect(result).to.equal('done');
+            expect(result).to.equal(data);
             done();
         });
     });
@@ -43,12 +45,13 @@ describe('hold cases', function() {
     it('basic with options object', function(done) {
 
         var hold = new Hold({ expires: 1000 });
+        var data = new Date().getTime();
         hold(function(until) {
 
-            until(null, 'done');
+            until(null, data);
         }, function(err, result) {
 
-            expect(result).to.equal('done');
+            expect(result).to.equal(data);
             done();
         });
     });
@@ -92,18 +95,19 @@ describe('hold cases', function() {
         var hold = new Hold();
         var workCount = 0;
         var resultCount = 0;
-
+        var data = new Date().getTime();
+        
         var work = function(until) {
 
             workCount++;
             setTimeout(function(u){
-                u(null, 'done');
+                u(null, data);
             }, 10, until);
         };
 
         var finish = function(err, result) {
             resultCount++;
-            expect(result).to.equal('done');
+            expect(result).to.equal(data);
         };
         
         // make multiple simulateous calls to hold
@@ -114,6 +118,96 @@ describe('hold cases', function() {
         setTimeout(function() {
             expect(workCount).to.equal(1); // only the first call did work
             expect(resultCount).to.equal(10); // all calls got the result from first call
+            done();
+        }, 20);
+    });
+    
+    it('on error use next caller', function(done) {
+
+        var hold = new Hold({ expires: 10 });
+        var data = new Date().getTime();
+        
+        hold(function(until) {
+
+            setTimeout(function(u){
+                u(new Error('fake error'));
+            }, 10, until);
+        }, function(err, result) {
+            
+            expect(err).to.exist;
+        });
+        
+        hold(function(until) {
+
+            setTimeout(function(u){
+                u(null, data);
+            }, 10, until);
+        }, function(err, result) {
+            
+            expect(err).to.not.exist;
+            expect(result).to.equal(data);
+        });
+        
+        hold(function(until) {
+
+            setTimeout(function(u){
+                u(null, new Date().getTime());
+            }, 2, until);
+        }, function(err, result) {
+            
+            expect(err).to.not.exist;
+            expect(result).to.equal(data);
+            done();
+        });
+    });
+    
+    it('concurrent errors', function(done) {
+
+        var hold = new Hold();
+        var workCount = 0;
+        var workFailedCount = 0;
+        var resultCount = 0;
+        var resultFailedCount = 0;
+        var data = new Date().getTime();
+        
+        var work = function(until) {
+
+            workCount++;
+            setTimeout(function(u){
+                u(null, data);
+            }, 10, until);
+        };
+        
+        var workFailed = function(until) {
+
+            workFailedCount++;
+            setTimeout(function(u){
+                u(new Error('fake error'));
+            }, 10, until);
+        };
+        
+        var finish = function(err, result) {
+            resultCount++;
+            expect(err).to.not.exist;
+            expect(result).to.equal(data);
+        };
+        
+        var finishFailed = function(err, result) {
+            resultFailedCount++;
+            expect(err).to.exist;
+        };
+        
+        // make multiple simulateous calls to hold
+        hold(workFailed, finishFailed);
+        for (var i = 0; i < 10; i++) {
+            hold(work, finish);
+        }
+
+        setTimeout(function() {
+            expect(workCount).to.equal(1);
+            expect(workFailedCount).to.equal(1);
+            expect(resultCount).to.equal(10);
+            expect(resultFailedCount).to.equal(1);
             done();
         }, 20);
     });
